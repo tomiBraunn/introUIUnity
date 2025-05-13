8e3a6cbf37fb845c1089a5264c368b5aceba7b7a
Tomi <141858115+tomiBraunn@users.noreply.github.com>
2025-05-13 09:01:51 -0300

    calculadora funciona

---
+++ IntroUI/Assets/sumaYResultado.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class sumaYResultado : MonoBehaviour
+{
+
+    public TMP_InputField inputNum1;
+    public TMP_InputField inputNum2;
+    public TextMeshProUGUI txtResultado;
+    int num1;
+    int num2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        txtResultado.text = "";
+    }
+
+    public void SumarYMostrar()
+    {
+        num1 = int.Parse(inputNum1.text);
+        num2 = int.Parse(inputNum2.text);
+        txtResultado.text = (num1 + num2).ToString();
+    }
+}
