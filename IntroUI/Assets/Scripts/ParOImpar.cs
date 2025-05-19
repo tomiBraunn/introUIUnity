53735c68190f528f285a3795f4f07d308d5d470d
Tomas B <tomasbraun2008@gmail.com>
2025-05-19 18:41:20 -0300

    terminado

---
+++ IntroUI/Assets/Scripts/ParOImpar.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class ParOImpar : MonoBehaviour
+{
+    public TMP_InputField inputNum1;
+    int num1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void DecirParImpar()
+    {
+        num1 = int.Parse(inputNum1.text);
+        if (num1 % 2 == 0)
+        {
+            Debug.Log(num1 + " es par");
+        }
+        else
+        {
+            Debug.Log(num1 + " es impar");
+        }
+    }
+}
---
+++ IntroUI/Assets/divisionProhibida.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class divisionProhibida : MonoBehaviour
+{
+    public TMP_InputField inputNum1;
+    public TMP_InputField inputNum2;
+    int num1;
+    int num2;
+    string txtResultado;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void Dividirnumeros()
+    {
+        num1 = int.Parse(inputNum1.text);
+        num2 = int.Parse(inputNum2.text);
+    
+        if (num2 == 0)
+        {
+            Debug.Log("No se puede dividir entre cero");
+        }
+        else
+        {
+            txtResultado = (num1 / num2).ToString();
+            Debug.Log(num1 + " entre " + num2 + " da " + txtResultado);
+        }
+    }
+}
+
